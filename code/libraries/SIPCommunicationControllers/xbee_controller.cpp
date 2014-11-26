6d0797bcdbab3843e78ccd09155f3ef727551653
eshughes <eshughes@ucsc.edu>
2014-11-25 21:45:46 -0800

    Adding Testing Doc
    Moved User Manual doc to documentation folder. Added testing doc. Also
    added xbee_controller.cpp. This file and the .h file however are not up
    to date - I will add the completed ones at a later date, I still have
    more edits and testing I would like to complete.

---
+++ code/libraries/SIPCommunicationControllers/xbee_controller.cpp
@@ -0,0 +1,30 @@
+//Implementation for xbee controller
+
+#include "xbee_controller.h"
+
+
+XBee::XBee(){
+  data_index = 0;
+  data_size = 100;
+}
+
+void XBee::send_int(int val){
+
+}
+
+void XBee::send_float(float val){
+
+}
+
+void XBee::receive_data(){
+
+}
+
+void XBee::interpret_data(){
+
+}
+
+void XBee::clear_data(){
+
+}
+
--- code/libraries/SIPCommunicationControllers/xbee_controller.h
+++ code/libraries/SIPCommunicationControllers/xbee_controller.h
@@ -3,6 +3,22 @@
 
 class XBee {
   // Class to interface with the XBee.
+  public:
+  //Transmitting functions
+  void send_int(int val);
+  void send_float(float val);
+  
+  //Receiving functions
+  void receive_data();
+  void interpret_data();
+  void clear_data();
+  
+  private:
+  char data_in[100]; //incoming data stream
+  int data_size;
+  int data_index;
+  char char_in;
+  char data_type;
 };
 
 #endif
