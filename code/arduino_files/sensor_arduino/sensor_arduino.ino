bf9e83b685b8a33c688565475c4e583cb04bdd2c
eshughes <eshughes@ucsc.edu>
2014-12-07 18:47:45 -0800

    Minor changes to sketch
    made displayed timestamp seconds instead of milliseconds

--- code/arduino_files/sensor_arduino/sensor_arduino.ino
+++ code/arduino_files/sensor_arduino/sensor_arduino.ino
@@ -22,50 +22,55 @@
 
 
 class SensorArduino {
-  public:
-    // Arduino variables
-    int m_sleep_time;
-    time_t timestamp;
-    // Components
-    MoistureSensor* moisture_sensor;
-    XBee* xbee_transmitter;
-    //... etc.
+public:
+  // Arduino variables
+  int m_sleep_time;
+  time_t timestamp;
+  // Components
+  MoistureSensor* moisture_sensor;
+  XBee* xbee_transmitter;
+  //... etc.
 
-    SensorArduino() {
-      m_sleep_time = 5000; // 5 seconds
-      timestamp = 0; 
-      /* Start at 0
-       * Timestamp value should later be updated to reflect actual time
-       * rather than time passed since arduino was turned on
-       */
+  SensorArduino() {
+    m_sleep_time = 5000; // 5 seconds
+    timestamp = 0; 
+    /* Start at 0
+     * Timestamp value should later be updated to reflect actual time
+     * rather than time passed since arduino was turned on
+     */
 
-      //Pins used by the SLHT5 moisture sensor.
-      int data_pin = 10;
-      int clock_pin = 11;
-      
-      // Initialize the components of SIP
-      //moisture_sensor = new TestSensor(MOISTURE_SENSOR_PIN_1);
-      moisture_sensor = new SLHT5Sensor(10, data_pin, clock_pin);
-      xbee_transmitter = new XBee();
-      // new solar controller? etc.
-    }
+    //Pins used by the SLHT5 moisture sensor.
+    int data_pin = 10;
+    int clock_pin = 11;
 
-    void loop(){
-      // Update the components.
-      moisture_sensor->update();
-      
-      //Serial.println("In main loop!");
-      // Send the new saturation percent over the network.
-      float saturation_percent = moisture_sensor->get_saturation();
-      float temperature = moisture_sensor->get_temperature();
-      xbee_transmitter->send_package(saturation_percent, temperature, timestamp);
-      timestamp += m_sleep_time/1000;
-      delay(m_sleep_time);
-    }
+    // Initialize the components of SIP
+    //moisture_sensor = new TestSensor(MOISTURE_SENSOR_PIN_1);
+    moisture_sensor = new SLHT5Sensor(10, data_pin, clock_pin);
+    xbee_transmitter = new XBee();
+    // new solar controller? etc.
+  }
+
+  void loop(){
+    // Update the components.
+    moisture_sensor->update();
+
+    //Serial.println("In main loop!");
+    // Send the new saturation percent over the network.
+    float saturation_percent = moisture_sensor->get_saturation();
+    float temperature = moisture_sensor->get_temperature();
+    xbee_transmitter->send_package(saturation_percent, temperature, timestamp);
+    timestamp += m_sleep_time/1000;
+    delay(m_sleep_time);
+  }
 };
 
 // Define the functions that the Arduino will call.
 SensorArduino* arduino;
-void setup(){ arduino =  new SensorArduino(); }
-void loop(){ arduino->loop(); }
+void setup(){ 
+  arduino =  new SensorArduino(); 
+}
+void loop(){ 
+  arduino->loop(); 
+}
+
 
