57e79d214b7f5e371a9ed6cfa85d78fe8366f8f2
eshughes <eshughes@ucsc.edu>
2014-12-07 17:50:01 -0800

    Updated water and sensor sketches
    Added libraries, added xbee functionality and makeshift timestamp

--- code/arduino_files/sensor_arduino/sensor_arduino.ino
+++ code/arduino_files/sensor_arduino/sensor_arduino.ino
@@ -2,59 +2,64 @@
 // cmps 115 Fall 2014
 
 #include <Arduino.h>
+#include <Time.h>
 
 // Include Data Stream files
-#include "generic_data_stream.h"
-#include "array_data_stream.h"
+#include <array_data_stream.h>
+#include <generic_data_stream.h>
+#include <stt_data_point.h>
 
 // Include Communication Controller files
-#include "generic_communication_controller.h"
-#include "serial_controller.h"
-//#include "xbee_controller.h"
-//#include <Time.h>
+#include <generic_communication_controller.h>
+#include <serial_controller.h>
+#include <xbee_controller.h>
 
 // Include Moisture Sensor files
-#include "generic_moisture_sensor.h"
-//#include "test_sensor.h"
-#include <SHT1x.h>
-#include "slht5_adafruit_sensor.h"
+#include <generic_moisture_sensor.h>
+#include <slht5_adafruit_sensor.h>
+#include <watermark_sensor.h>
+
 
 
 class SensorArduino {
   public:
     // Arduino variables
     int m_sleep_time;
-
+    time_t timestamp;
     // Components
     MoistureSensor* moisture_sensor;
-    CommunicationController* communication_controller;
+    XBee* xbee_transmitter;
     //... etc.
 
     SensorArduino() {
       m_sleep_time = 100; // 1 second
+      timestamp = 0; 
+      /* Start at 0
+       * Timestamp value should later be updated to reflect actual time
+       * rather than time passed since arduino was turned on
+       */
 
-      // Pins used by the SLHT5 moisture sensor.
+      //Pins used by the SLHT5 moisture sensor.
       int data_pin = 10;
       int clock_pin = 11;
       
       // Initialize the components of SIP
       //moisture_sensor = new TestSensor(MOISTURE_SENSOR_PIN_1);
       moisture_sensor = new SLHT5Sensor(data_pin, clock_pin);
-      communication_controller = new SerialController();
-			// new solar controller? etc.
+      xbee_transmitter = new XBee();
+      // new solar controller? etc.
     }
 
     void loop(){
       // Update the components.
       moisture_sensor->update();
-      communication_controller->update();
+      
       
       // Send the new saturation percent over the network.
       float saturation_percent = moisture_sensor->get_saturation();
-      communication_controller->send_saturation_level(saturation_percent);
       float temperature = moisture_sensor->get_temperature();
-      communication_controller->send_temperature(temperature);
-
+      xbee_transmitter->send_package(saturation_percent, temperature, timestamp);
+      timestamp += m_sleep_time;
       delay(m_sleep_time);
     }
 };
--- code/arduino_files/water_arduino/water_arduino.ino
+++ code/arduino_files/water_arduino/water_arduino.ino
@@ -2,20 +2,21 @@
 // cmps 115 Fall 2014
 
 #include <Arduino.h>
-
+#include <Time.h>
 // Include Data Stream files
-#include "generic_data_stream.h"
-#include "array_data_stream.h"
+#include <array_data_stream.h>
+#include <generic_data_stream.h>
+#include <stt_data_point.h>
 
 // Include Communication Controller files
-#include "generic_communication_controller.h"
-#include "serial_controller.h"
-//#include "xbee_controller.h"
-//#include <Time.h>
+#include <generic_communication_controller.h>
+#include <serial_controller.h>
+#include <xbee_controller.h>
+
 
 // Include Water Controller files
-#include "generic_water_controller.h"
-#include "test_water_controller.h"
+#include <generic_water_controller.h>
+#include <test_water_controller.h>
 
 
 class WaterArduino {
@@ -23,9 +24,9 @@ class WaterArduino {
     // Arduino variables
     int m_sleep_time; // 1 seconds
     int m_testing_pin; // For testing... Light up the pin 13 blue LED
-  
     // Components
     WaterController* water_controller;
+    XBee* xbee_receiver;
     CommunicationController* communication_controller;
     //... etc.
   
@@ -37,18 +38,20 @@ class WaterArduino {
       water_controller = new TestWaterController(m_testing_pin);
 			//communication_controller = new SerialController();
 			// new solar controller? wifi_controller? etc.
-  
+      xbee_receiver = new XBee();
     }
 
     void loop(){
       // Update the components if necessary.
-  
-			// Somehow check the communication controller for new data
-			// Somehow check the moisture value to see if it is too dry.
-			
-			// Water the plants if the ground is too dry.
+      // Somehow check the communication controller for new data
+      //Somehow check the moisture value to see if it is too dry.
+      // Water the plants if the ground is too dry.
       water_controller->water_plants();
-
+      
+      //Check if package is received while calling receival function.            
+      if(!xbee_receiver->receive_package()){
+        Serial.println("No package received.");
+      }
       delay(m_sleep_time);
     }
 };
